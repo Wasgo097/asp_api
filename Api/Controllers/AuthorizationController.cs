3f10326852b51f5c9187f817f6240def6859c188
Wasgo097 <patrpak906@student.polsl.pl>
2020-06-17 19:16:00 +0200

    added userlogin model and change usercontr to authocontr

--- Api/Controllers/AuthorizationController.cs
+++ Api/Controllers/AuthorizationController.cs
@@ -2,29 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
-using Api.Models;
-using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Authorization;
 
 namespace Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class AuthorizationController : ControllerBase
     {
         SqliteConnection connection;
-        List<User>users;
-        public UsersController()
+        List<User> users;
+        public AuthorizationController()
         {
             connection = new SqliteConnection("Data Source=mydb.db");
             users = new List<User>();
             fill_list();
         }
         // GET api/values
-        [HttpGet,Authorize]
+        [HttpGet, Authorize]
         [EnableCors("developerska")]
         //public ActionResult<string> Get()
         public ActionResult<IEnumerable<User>> Get()
@@ -56,7 +56,7 @@ namespace Api.Controllers
                 string _login = (string)data[1];
                 string _password = (string)data[2];
                 string _type = (string)data[3];
-                users.Add(new User{ Id = _id,Login=_login,Password=_password,Type=_type });
+                users.Add(new User { Id = _id, Login = _login, Password = _password, Type = _type });
             }
             order_id();
             connection.Close();
@@ -74,7 +74,7 @@ namespace Api.Controllers
             sql_cmd.ExecuteNonQuery();
             foreach (var user in users)
             {
-                string cmd_ins = "insert into users(id,login,password,type) values(" + user.Id.ToString() + ",'" + user.Login + "','" + user.Password + "','"+user.Type+"');";
+                string cmd_ins = "insert into users(id,login,password,type) values(" + user.Id.ToString() + ",'" + user.Login + "','" + user.Password + "','" + user.Type + "');";
                 SqliteCommand sql_cmd_in = new SqliteCommand(cmd_ins, connection);
                 sql_cmd_in.ExecuteNonQuery();
             }
